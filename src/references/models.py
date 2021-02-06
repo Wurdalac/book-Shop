3bc14b186b75421c9d196916e3892497c43cc66d
Wurdalac <ooo4ko00@mail.ru>
2021-02-06 19:39:50 +0200

    models

--- src/references/models.py
+++ src/references/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(
+        verbose_name="City's name",
+        max_length=20,
+        )
+    description = models.TextField(
+        verbose_name="City's description",
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name 
+
+
+class News(models.Model):
+    title = models.CharField(verbose_name='Title', max_length=30)
+    anons = models.CharField(verbose_name='Anons', max_length=250)
+    description= models.TextField(verbose_name='Description')
+    date = models.DateTimeField(verbose_name='Publication date')
+    
+    def __str__(self):
+        return self.title 
+    
+   
